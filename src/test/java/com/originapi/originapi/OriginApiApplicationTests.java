f6addc2a2b8607f2bb17e0b9089b506b29cd6124
Anderson Guanche <anderson7guanche@gmail.com>
2025-11-01 00:41:04 +0100

    Etapa 1: configuracion de SpringBoot y preparacion de IDE

---
+++ src/test/java/com/originapi/originapi/OriginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.originapi.originapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
